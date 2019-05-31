7f4be9db49e5d2cce7c484f6859f0a9238fadcc3
Raphael Pampolha <ralepa@gmail.com>
2019-05-30 23:03:17 -0300

    Adiciona classe de request de transação

---
+++ acquirer/Request/TransactionRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace acquirer.Request
+{
+    public class TransactionRequest
+    {
+        public decimal Valor { get; set; }
+        public string Adquirente { get; set; }
+        public string Bandeira { get; set; }
+        public string Tipo
+
+
+        public TransactionRequest()
+        {
+        }
+    }
+}
